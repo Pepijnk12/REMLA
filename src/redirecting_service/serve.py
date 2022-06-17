3dbb02c78cfff3025379af958ac291be9041f81c
quintene <q@qntn.nl>
2022-06-17 20:26:28 +0200

    linting fix

--- src/redirecting_service/serve.py
+++ src/redirecting_service/serve.py
@@ -7,7 +7,6 @@ from flask import Flask, jsonify, request, render_template
 from flasgger import Swagger
 from flask_cors import CORS
 from kubernetes import client, config
-from utils import pod_name_to_model_version
 
 config.load_incluster_config()
 
@@ -22,6 +21,14 @@ state = {
 
 posts = []
 models = set()
+
+
+MODEL_PREFIX = "stackoverflow-tag-pred-model-"
+
+def pod_name_to_model_version(pod_name: str):
+    """e.g stackoverflow-tag-pred-model-1-4-0-74cbcb4b6-68gbg"""
+    version_list = pod_name[len(MODEL_PREFIX):].split("-")[:3]
+    return '.'.join(version_list)
 
 
 @app.route('/', methods=['GET'])
